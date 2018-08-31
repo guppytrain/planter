d65b52a8630c8cc36080e078b6c51c63520037ae
Guppy Train @ Legion - Ubuntu <guppytrain.login@gmail.com>
2018-08-30 17:38:01 -0700

    fix bug with false positive

--- add_ssh_key.sh
+++ add_ssh_key.sh
@@ -3,7 +3,7 @@
 # NOTE: assumes default rsa key names
 id_file="$HOME/.ssh/id_rsa"
 
-# create key as needed
+# create folder as needed
 if [ ! -d "$HOME/.ssh" ]; then
     echo "Creating new .ssh folder"
 
@@ -15,11 +15,22 @@ else
     echo "Found existing .ssh folder"
 fi
 
+# create key as needed
+if [ ! -f "${id_file}" ]; then
+    echo "Creating new rsa key"
+
+    [ -z "$1" ] && ssh-keygen -t rsa -b 4096 -C "$1" \
+        || ssh-keygen -t rsa -b 4096
+else
+    echo "Found existing rsa key"
+    cat "${id_file}.pub"
+fi
+
 # get fingerprint
 fp="$(ssh-keygen -lf $id_file | awk '{ print $2}')"
 
 # check against added list
-match="$(ssh-add -l | grep $fp)"
+match="$(ssh-add -l | grep -o "${fp}")"
 
 # if not found
 if [ -z "$match" ]; then
